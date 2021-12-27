005015a76d9bccd979c1ae68fc2b7b5cd5e10b68
Troy Dorman <troydorman@gmail.com>
2021-12-27 14:00:09 -0700

    connected to the database

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger-output.json");
+const connectDB = require("./config/db");
 
 app.use(express.json({ extended: false }));
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+//connect to mongoose
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
