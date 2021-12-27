14a638af0a326f8290ef8be5bdf368d5c76a9dae
Troy Dorman <troydorman@gmail.com>
2021-12-27 12:06:53 -0700

    integrated wagger documentation generator

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
 
 app.use(express.json({ extended: false }));
 
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+  console.log("API documentation: http://localhost:3000/doc");
+});
+
+/* Endpoints */
+require("./src/endpoints")(app);
