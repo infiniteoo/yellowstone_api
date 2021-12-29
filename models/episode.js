4190cde5648b421e2bef1749020d1fc2b0dc6153
Troy Dorman <troydorman@gmail.com>
2021-12-28 17:01:24 -0700

    created episode schema for our database

---
+++ models/episode.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+// Character Schema
+const episodeSchema = mongoose.Schema({
+  title: {
+    type: String,
+  },
+  season: {
+    type: Number,
+  },
+  episode: {
+    type: Number,
+  },
+  air_date: {
+    type: String,
+  },
+  characters: {
+    type: Array,
+  },
+  series: {
+    type: String,
+  },
+});
+
+const Episode = (module.exports = mongoose.model("Episode", episodeSchema));
+
+// get Episodes
+module.exports.getEpisodes = (cb, limit) => {
+  Episode.find(cb).limit(limit);
+};
+
+// get single Episode
+module.exports.getEpisodeById = (id, cb) => {
+  Episode.findById(id, cb);
+};
+
+// add a Episode
+module.exports.addEpisode = (episode, cb) => {
+  Episode.create(episode, cb);
+};
+
+// update a Character
+module.exports.updateEpisode = (id, episode, options, cb) => {
+  const query = { _id: id };
+  const update = {
+    title: episode.title,
+    genre: episode.genre,
+    description: episode.description,
+    author: episode.author,
+    publisher: episode.publisher,
+    pages: episode.pages,
+    image_url: episode.image_url,
+    buy_url: episode.buy_url,
+  };
+
+  Episode.findOneAndUpdate(query, update, options, cb);
+};
+
+// delete Character
+module.exports.removeEpisode = (id, cb) => {
+  const query = { _id: id };
+  Episode.remove(query, cb);
+};
