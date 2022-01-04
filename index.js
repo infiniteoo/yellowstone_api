0179dc82049b22ba92bef4dc51f329546931981f
Troy Dorman <troydorman@gmail.com>
2022-01-04 11:34:20 -0700

    small typo updates to host url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
-  console.log("API documentation: http://localhost:3000/doc");
+  console.log("API documentation: http://localhost:5000/doc");
 });
 
 /* Endpoints */
